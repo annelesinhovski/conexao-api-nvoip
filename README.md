c5fc2d494418457a6eaf1b2e064f6477676aa4e6
Anne Lesinhovski <you@example.com>
2021-09-07 18:38:12 -0300

    feat: projeto conexao api voip

---
+++ README.md
@@ -0,0 +1,23 @@
+# Exemplo de integração com a Nova API da Nvoip
+
+## Arquitetura
+
+Este é um projeto bem simples, com a estrutura:
+
+- **/src/script.js**
+  Aqui estão os scripts para fazer a integração com a API via javascript.
+- **/index.html**
+  Homepage com exemplos de Buttons que chamam as funções e eXecutam as ações para integrar com a API.
+
+### Adicional
+
+- **Fetch API do Javascript**: para fazer as requisições HTTP
+- **Autenticação usando OAuth2**: protocolo de autorização aceito pela API integrada
+
+## Sobre a Nova API da Nvoip
+
+Mais informações sobre a Nova API da Nvoip: [Link](https://www.nvoip.com.br/api/?utm_source=social&utm_medium=influencers&utm_campaign=apiv2&utm_term=techanne)
+
+Link da documentação da API: [Link](https://nvoip.docs.apiary.io/)
+
+Para fazer testes de **envio de SMS, ligações e torpedo de voz**, você precisa criar uma conta grátis da Nvoip na plataforma, por meio [deste link](https://painel.nvoip.com.br/signup/?referralid=175118). Também é na plataforma que você vai obter seus dados de **numbersip e usertoken** para autenticar seu acesso a API.
---
+++ src/script.js
@@ -0,0 +1,60 @@
+// // SETANDO O HEADER DA REQUISIÇÃO
+const defaultHeaders = { 'Content-Type': 'application/json' };
+const authorizationHeaders = {
+  Authorization: 'Basic TnZvaXBBcGlWMjpUblp2YVhCQmNHbFdNakl3TWpFPQ==',
+  'Content-Type': 'application/x-www-form-urlencoded',
+};
+
+// SETANDO O BODY DA REQUISIÇÃO
+const number_ip = '123456789'; // gerar esse número na plataforma da NVoip
+const user_token = 'xxxxxxxx'; // gerar esse número na plataforma da NVoip
+
+const credentials = `username=${number_ip}&password=${user_token}&grant_type=password`;
+
+// CONECTANDO COM A PLATAFORMA DA NVOIP
+
+function conectApi() {
+  fetch('https://api.nvoip.com.br/v2/oauth/token', {
+    method: 'POST',
+    headers: { ...defaultHeaders, ...authorizationHeaders },
+    body: credentials,
+  })
+    .then((res) => res.json()) //  retorna a resposta da requisição em formato JSON
+    .then((data) => {
+      access_token = data['access_token']; //  acessa o access_token
+    })
+    .catch((error) => console.log(error)); //  caso ocorra um erro na requisição
+}
+
+// OBTER INFORMAÇÕES DE TOKEN
+
+function getToken() {
+  const options = {
+    method: 'POST',
+    headers,
+    body: `token=${access_token}`,
+  };
+
+  fetch('https://api.nvoip.com.br/v2/oauth/check_token', options).then((res) =>
+    res.json()
+  );
+}
+
+// SETUP PARA ENVIAR SMS
+
+function sendSms(numberPhone, message, flashSms = false) {
+  const oAuthHeaders = { Authorization: `Bearer ${access_token}` };
+
+  fetch('https://api.nvoip.com.br/v2/sms', {
+    method: 'POST',
+    headers: { ...defaultHeaders, ...oAuthHeaders },
+    body: JSON.stringify({
+      numberPhone,
+      message,
+      flashSms,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+}
